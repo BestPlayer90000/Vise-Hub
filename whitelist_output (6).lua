803ff57e919db6dd9b557cdda3efc5508be24aed
Iliekbal <89629990+Iliekbal@users.noreply.github.com>
2023-08-04 22:00:22 -0400

    Add files via upload

---
+++ whitelist_output (6).lua
@@ -0,0 +1,8 @@
+
+
+
+
+
+
+
+"ERROR UnexpectedValueException: Firebase\\JWT\\ExpiredException: Expired token in \/home\/tgajsndi\/public_html\/Whitelist\/vendor\/firebase\/php-jwt\/src\/JWT.php:158\nStack trace:\n#0 \/home\/tgajsndi\/public_html\/Whitelist\/core\/firebaseAuth.php(27): Firebase\\JWT\\JWT::decode()\n#1 \/home\/tgajsndi\/public_html\/Whitelist\/core\/firebaseAuth.php(204): verify_firebase_token()\n#2 \/home\/tgajsndi\/public_html\/Whitelist\/api\/fileHandlerV11.php(84): get_firebase_token_data()\n#3 {main}"Account not verified - contact support
